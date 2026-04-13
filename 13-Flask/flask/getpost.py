0e67209a9663ec5cc2d884d53e8aa1b5f49b65b2
nidhikumari244 <nidhigupta2462@gmail.com>
2026-04-13 23:19:06 +0530

    FLASK

--- 13-Flask/flask/getpost.py
+++ 13-Flask/flask/getpost.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request 
 
 # ...
 # It create an instance of the flask class,
@@ -10,13 +10,21 @@ app = Flask(__name__)
 def welcome():
     return "<html><H1>welcome to this flask work.its amazing</H1></html>"
 
-@app.route("/index")
+@app.route("/index",method=['GET'])
 def index():
     return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/form',method=['GET','POST'])
+def form():
+    if request.method=='POST':
+      name=request.method['name']
+      return f'Hello{name}!'  
+    return render_template('form.html')
+
 
 
 if __name__=="__main__":
